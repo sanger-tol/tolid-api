f4d63a28bdbab4893fc4f399b45250cf1493baba
Kenneth Haug <khaug@yahoo.com>
2020-09-26 15:34:07 +0100

    Revert "First save of the code. It's just really a "generated brainstorm" in a non working state."
    This reverts commit 12ad463fae310bf57c5c604175f3a4add1e231c4.

--- swagger_client/api/__init__.py
+++
@@ -1,7 +0,0 @@
-from __future__ import absolute_import
-
-# flake8: noqa
-
-# import apis into api package
-from swagger_client.api.consumers_api import ConsumersApi
-from swagger_client.api.curators_api import CuratorsApi
--- utils/db_utils.py
+++
@@ -1,8 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-def get_db(db=None):
-    if not db:
-        db = SQLAlchemy()
-
-    return db
-    
