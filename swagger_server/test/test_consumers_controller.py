f076c8ba3058d2a4d0233f160184429b196f0f4b
Andrew Varley <av10@sanger.ac.uk>
2020-10-21 13:47:03 +0100

    Initial working version of tests

--- swagger_server/test/test_consumers_controller.py
+++ swagger_server/test/test_consumers_controller.py
@@ -12,21 +12,71 @@ from swagger_server.test import BaseTestCase
 class TestConsumersController(BaseTestCase):
     """ConsumersController integration test stubs"""
 
+    def setUp(self):
+        #TODO
+        pass
+
+    def tearDown(self):
+        #TODO
+        pass
+
     def test_search_public_name(self):
         """Test case for search_public_name
 
         searches DToL public names
         """
-        query_string = [('taxonomyId', '1234'),
-                        ('specimenId', '5678'),
-                        ('skip', 1),
-                        ('limit', 10000000)]
+        # No taxonomyId given
+        query_string = []
         response = self.client.open(
-            '/public-name',
+            '/public_name_api/public-name',
             method='GET',
             query_string=query_string)
+        self.assert400(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+        # No specimenId given
+        query_string = [('taxonomyId', '6344')]
+        response = self.client.open(
+            '/public_name_api/public-name',
+            method='GET',
+            query_string=query_string)
+        self.assert400(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+        # Taxonomy ID not in database
+        query_string = [('taxonomyId', '999999999'),
+                        ('specimenId', 'SAN0000100')]
+        response = self.client.open(
+            '/public_name_api/public-name',
+            method='GET',
+            query_string=query_string)
+        #TODO
+        #self.assert400(response,
+        #               'Response body is : ' + response.data.decode('utf-8'))
+
+        # All data given
+        query_string = [('taxonomyId', '6344'),
+                        ('specimenId', 'SAN0000100')]
+        response = self.client.open(
+            '/public_name_api/public-name',
+            method='GET',
+            query_string=query_string)
+        expect = [{
+            "commonName": "lugworm",
+            "family": "Arenicolidae",
+            "genus": "Arenicola",
+            "order": "None",
+            "phylum": "Annelida",
+            "prefix": "wuAreMari",
+            "publicName": "wuAreMari1",
+            "species": "Arenicola marina",
+            "specimenId": "SAN0000100",
+            "taxaClass": "Polychaeta",
+            "taxonomyId": 6344
+        }]
         self.assert200(response,
                        'Response body is : ' + response.data.decode('utf-8'))
+        self.assertEquals(expect, response.json)
 
 
 if __name__ == '__main__':
