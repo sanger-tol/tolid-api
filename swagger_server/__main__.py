86023f9e8fb2d4231b849972a2536bf1a14f6b90
Andrew Varley <av10@sanger.ac.uk>
2020-10-22 14:57:58 +0100

    API now works with SQLAlchemy

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -10,11 +10,11 @@ def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'Tree of Life public name API'}, pythonic_params=True)
-    app.run(port=8080)
-
     app.app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
     app.app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app.app)
+    app.run(port=8080)
+
 
 if __name__ == '__main__':
     main()
