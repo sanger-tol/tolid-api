e83fe743b602ba705af5806e1a69fae115445dcb
Andrew Varley <av10@sanger.ac.uk>
2021-02-16 15:01:31 +0000

    Added test for accepting requests

--- tolid-ui/app/src/components/requestsList/RequestsList.test.tsx
+++ tolid-ui/app/src/components/requestsList/RequestsList.test.tsx
@@ -31,17 +31,49 @@ it("successful request", async () => {
       }
     }
   ];
+  const fakeAccept = [{
+    "species": {
+      "commonName": "None",
+      "family": "Nereididae",
+      "genus": "Perinereis",
+      "kingdom": "Metazoa",
+      "order": "Phyllodocida",
+      "phylum": "Annelida",
+      "prefix": "wpPerVanc",
+      "scientificName": "Perinereis vancaurica",
+      "taxaClass": "Polychaeta",
+      "taxonomyId": 6355,
+  },
+    "specimen": {
+        "specimenId": "SAN0000200"
+    },
+    "tolId": "mHomSap4"
+}]
+  const fakeResults2 = [];
 
   jest.spyOn(global, "fetch").mockImplementationOnce(() =>
       Promise.resolve({
       json: () => Promise.resolve(fakeResults),
       ok: true
-    })
-    );
+    })).mockImplementationOnce(() =>
+      Promise.resolve({
+      json: () => Promise.resolve(fakeAccept),
+      ok: true
+    })).mockImplementationOnce(() =>
+    Promise.resolve({
+    json: () => Promise.resolve(fakeResults2),
+    ok: true
+  }));
 
   render(<RequestsList/>);
   expect(await screen.findByText(/Perinereis vancaurica/i)).toBeInTheDocument();
   expect(global.fetch).toHaveBeenCalledWith('/api/v2/requests/pending', {"headers": {"Content-Type": "application/json", "api-key": "1234"}, "method": "GET"});
+
+  await act(async () => {
+    const acceptButton = screen.queryByText('Accept')
+    userEvent.click(acceptButton)
+  });  
+  expect(await screen.findByText(/ToLID mHomSap4 has been assigned/i)).toBeInTheDocument();
 
   // remove the mock to ensure tests are completely isolated
   global.fetch.mockRestore();
--- tolid-ui/app/src/components/requestsList/RequestsList.tsx
+++ tolid-ui/app/src/components/requestsList/RequestsList.tsx
@@ -8,7 +8,8 @@ export interface Props {
 }
 export interface State {
     requests: Request[],
-    error: ErrorMessage | null
+    error: ErrorMessage | null,
+    message: string
 }
 
 class RequestsList extends React.Component<Props, State> {
@@ -16,11 +17,16 @@ class RequestsList extends React.Component<Props, State> {
     super(props);
     this.state = {
       requests: [],
-      error: null
+      error: null,
+      message: ''
     }
   }
   
   componentDidMount() {
+    this.updateRequestsList();
+  }
+
+  updateRequestsList() {
     fetch('/api/v2/requests/pending', {method: 'GET',
                             headers: {
                               'Content-Type': 'application/json',
@@ -45,6 +51,58 @@ class RequestsList extends React.Component<Props, State> {
       })
   }
 
+  acceptRequest = (event: any) => {
+    const requestId = event.target.dataset["request-id"];
+    fetch('/api/v2/requests/'+requestId+'/accept', {method: 'PATCH',
+                            headers: {
+                              'Content-Type': 'application/json',
+                              'api-key': '1234'
+                            }})
+        // the JSON body is taken from the response
+        .then(res => {
+          return res.json();
+        })
+        .then(res => {
+          if ("detail" in res) {
+            this.setState({
+              error: res
+            })
+          } else {
+            this.setState({
+              error: null,
+              message: "ToLID "+res[0].tolId+" has been assigned"
+            })
+            this.updateRequestsList();
+          }
+      })    
+  }
+
+  rejectRequest = (event: any) => {
+    const requestId = event.target.dataset["request-id"];
+    fetch('/api/v2/requests/'+requestId+'/reject', {method: 'PATCH',
+                            headers: {
+                              'Content-Type': 'application/json',
+                              'api-key': '1234'
+                            }})
+        // the JSON body is taken from the response
+        .then(res => {
+          return res.json();
+        })
+        .then(res => {
+          if ("detail" in res) {
+            this.setState({
+              error: res
+            })
+          } else {
+            this.setState({
+              error: null
+            })
+            this.updateRequestsList();
+          }
+      })    
+  }
+
+
   public render() {
       return (
         <div id="requestsList">
@@ -53,11 +111,16 @@ class RequestsList extends React.Component<Props, State> {
                 {this.state.error.title}: {this.state.error.detail}
             </p>
             }
+            {(this.state.message) &&
+            <p className="alert alert-success">
+                {this.state.message}
+            </p>
+            }
           
           {this.state.requests.length > 0 &&
             <table className="table table-striped">
               <thead>
-                <tr><th>Request ID</th><th>Taxonomy ID</th><th>Name</th><th>Requested by</th></tr>
+                <tr><th>Request ID</th><th>Taxonomy ID</th><th>Name</th><th>Specimen ID</th><th>Requested by</th><th>Action</th></tr>
               </thead>
               <tbody>
                 {this.state.requests.map((item: Request) => (
@@ -65,7 +128,14 @@ class RequestsList extends React.Component<Props, State> {
                     <td>{item.id}</td>
                     <td>{item.species.taxonomyId}</td>
                     <td>{item.species.scientificName}</td>
+                    <td>{item.specimen.specimenId}</td>
                     <td>{item.createdBy.name}</td>
+                    <td>
+                      {item.species.scientificName &&
+                        <button className="btn btn-sm btn-success" onClick={this.acceptRequest} data-request-id={item.id}>Accept</button>
+                      }
+                      <button className="btn btn-sm btn-danger" onClick={this.rejectRequest} data-request-id={item.id}>Reject</button>
+                    </td>
                   </tr>
                   ))}
               </tbody>
