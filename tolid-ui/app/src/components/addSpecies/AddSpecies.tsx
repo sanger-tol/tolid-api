a74610c89332bee766a68c915fb737eafb588d66
Edward Moulsdale <em29@sanger.ac.uk>
2021-08-16 15:04:01 +0000

    Extremely rough implementation of error messages

--- tolid-ui/app/src/components/addSpecies/AddSpecies.tsx
+++ tolid-ui/app/src/components/addSpecies/AddSpecies.tsx
@@ -1,6 +1,6 @@
 import { Species } from '../../models/Species';
 import * as React from 'react';
-import { ErrorMessage } from '../../models/ErrorMessage'
+import { ErrorMessage } from '../../models/ErrorMessage';
 import { httpClient } from '../../services/http/httpClient';
 
 import './AddSpecies.scss'
@@ -8,7 +8,6 @@ import './AddSpecies.scss'
 export interface AddSpeciesProps {
 }
 export interface AddSpeciesState {
-    input: string
 }
 
 const splitInput = (input: string) => {
@@ -18,8 +17,7 @@ const splitInput = (input: string) => {
     return input.split(regExp);
 }
 
-const parseInput = (input: string): Species => {
-    const split = splitInput(input);
+const parseInput = (split: string[]): Species => {
     const species = {
         prefix: split[0],
         scientificName: split[1],
@@ -34,27 +32,96 @@ const parseInput = (input: string): Species => {
     return species;
 }
 
-const postSpecies = (species: Species) => {
-    return httpClient().post('/species', species)
-        .then((data: any) => {
-            return data.data[0] as Request;
-        })
-        .then((err: any) => {
-            return err as ErrorMessage;
-        });
-}
-
 class AddSpecies extends React.Component<AddSpeciesProps, AddSpeciesState> {
-    sendRequest() {
-        const species = parseInput(this.state.input);
-        const err = postSpecies(species)
-        console.log(err);
+    constructor(props: AddSpeciesProps) {
+        super(props);
+        this.state = {
+            error: null
+        };
+    }
+
+    postSpecies = async (species: Species) => {
+        // return an empty error message, can't possibly return null due to a bug
+        // in the typescript compiler causing inference that it's always null,
+        // thus preventing compilation
+        var errorMessage = {
+            detail: "",
+            title: ""
+        }
+        await httpClient().post('/species', species)
+            .then((data: any) => {
+                // do nothing yet
+            })
+            .catch(
+                (err: any) => {
+                    errorMessage = err.response.data as ErrorMessage
+                }
+            )
+        return errorMessage;
+    }
+
+    getInputError = (split: string[]) => {
+        if (split.length !== 9) {
+            return "9 entries must be provided";
+        }
+        try {
+            parseInt(split[2]);
+        } catch (exception) {
+            return "The taxonomy ID (3rd entry) must be an integer";
+        }
+        // all good, return null
+        return null;
+    }
+
+    sendRequest = async (event: any) => {
+        event.preventDefault();
+        const input = document.getElementById("add-species-input") as HTMLInputElement;
+        const form = document.getElementById("add-species-form") as HTMLFormElement;
+        const split = splitInput(input.value);
+        // client side validation
+        const error = this.getInputError(split);
+        if (error !== null) {
+            this.showErrorMessage(input, error);
+            return;
+        }
+        // parse the species and POST
+        const species = parseInput(split);
+        const errorMessageDetail = (await this.postSpecies(species)).detail;
+        // server side validation
+        if (errorMessageDetail !== "") {
+            this.showErrorMessage(input, errorMessageDetail);
+            return;
+        }
+        // set the success
+        this.setSuccess(input, form);
+    }
+
+    showErrorMessage = (input: HTMLInputElement, error: string) => {
+        input.classList.add("is-invalid");
+        input.setCustomValidity(error);
+        input.reportValidity();
+    }
+
+    setSuccess = (input: HTMLInputElement, form: HTMLFormElement) => {
+        // reset the form
+        input.classList.remove("is-invalid");
+        form.reset();
     }
 
     public render() {
         return (
-            <div>
-                <button className="btn btn-primary" id="makeRequestButton" onClick={this.sendRequest}></button>
+            <div id="add-species-container">
+                <form className="form" id="add-species-form">
+                    <div className="form-group">
+                        <input type="text" className="form-control form-control-lg" id="add-species-input" placeholder="Species data..." />
+                        <small className="form-text text-muted">
+                            Add a new species to the database.
+                        </small>
+                    </div>
+                    <button className="btn btn-primary" id="add-species-button" onClick={this.sendRequest}>
+                        Add Species
+                    </button>
+                </form>
             </div>
         );
     }
--- tolid-ui/app/src/pages/Admin.tsx
+++ tolid-ui/app/src/pages/Admin.tsx
@@ -1,9 +1,11 @@
 import React from "react";
-import RequestsList from "../components/requestsList/RequestsList"
+import { Tab, Tabs } from "react-bootstrap";
+import RequestsList from "../components/requestsList/RequestsList";
+import AddSpecies from '../components/addSpecies/AddSpecies';
 
 function Admin() {
   return (
-    <div className="request">
+    <div id="admin-container">
       <header className="masthead text-center text-white">
         <div className="masthead-content">
           <div className="container">
@@ -15,16 +17,32 @@ function Admin() {
         <div className="bg-circle-3 bg-circle"></div>
         <div className="bg-circle-4 bg-circle"></div>
       </header>
-      <section>
-        <div className="container">
-          <div className="row align-items-center">
-            <div className="col-lg-12 order-lg-1">
-                <h2>Pending requests</h2>
-                <RequestsList/>
+      <Tabs defaultActiveKey="request" id="uncontrolled-tab-example" className="ml-auto">
+        <Tab eventKey="request" title="Pending Requests">
+          <section>
+            <div className="container">
+              <div className="row align-items-center">
+                <div className="col-lg-12 order-lg-1">
+                    <h2>Pending requests</h2>
+                    <RequestsList/>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-      </section>
+          </section>
+        </Tab>
+        <Tab eventKey="add-species" title="Add Species">
+          <section>
+            <div className="container">
+              <div className="row align-items-center">
+                <div className="col-lg-12 order-lg-1">
+                    <h2>Add Species</h2>
+                    <AddSpecies/>
+                </div>
+              </div>
+            </div>
+          </section>
+        </Tab>
+      </Tabs>
     </div>
   );
 }
