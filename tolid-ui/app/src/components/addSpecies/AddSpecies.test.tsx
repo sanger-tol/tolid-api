0f8e08020e85d0bd6924b8d1f12ca72ed7c8c07f
Edward Moulsdale <em29@sanger.ac.uk>
2021-08-17 12:17:37 +0000

    Removed useless import

--- tolid-ui/app/src/components/addSpecies/AddSpecies.test.tsx
+++ tolid-ui/app/src/components/addSpecies/AddSpecies.test.tsx
@@ -1,6 +1,5 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { example } from 'yargs';
 import AddSpecies from './AddSpecies';
 
 afterEach(() => {
