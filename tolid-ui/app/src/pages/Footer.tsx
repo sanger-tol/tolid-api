25d5893dc172baca4d99e7a2cc67d74882f82b0f
Kiernan Harding <kh16@sanger.ac.uk>
2021-09-29 16:57:20 +0100

    Fixed footer text spacing

--- tolid-ui/app/src/pages/Footer.tsx
+++ tolid-ui/app/src/pages/Footer.tsx
@@ -6,9 +6,9 @@ function Footer() {
       <footer className="py-2 bg-black fixed-bottom">
         <div className="container">
         <p className="m-0 text-center text-white small">Maintained by
-            <a href="https://sanger.ac.uk">Wellcome Sanger Institute</a>
-            <a href="https://www.sanger.ac.uk/programme/tree-of-life/">Tree of Life programme</a>
-            <a href="mailto:tol-platforms@sanger.ac.uk">Enabling Platforms team</a>
+            <a href="https://sanger.ac.uk"> Wellcome Sanger Institute</a>
+            , <a href="https://www.sanger.ac.uk/programme/tree-of-life/">Tree of Life programme</a>
+            , <a href="mailto:tol-platforms@sanger.ac.uk">Enabling Platforms team</a>
           </p>
         </div>
       </footer>
