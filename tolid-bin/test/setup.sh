ab381bad6f581702adc05f01e597737f79845a5d
Edward Moulsdale <em29@sanger.ac.uk>
2021-12-20 14:27:33 +0000

    Added teardown script

---
+++ tolid-bin/test/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# SPDX-FileCopyrightText: 2021 Genome Research Ltd.
+#
+# SPDX-License-Identifier: MIT
+
+# set the env file to default to .env.dev if no argument specified
+TEST_ENV_FILE="${1:-.env.dev}"
+
+# start everything
+docker-compose --env-file "$TEST_ENV_FILE" up -d --build tolid-api tolid-ui tolid-db
---
+++ tolid-bin/test/teardown.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# SPDX-FileCopyrightText: 2021 Genome Research Ltd.
+#
+# SPDX-License-Identifier: MIT
+
+# set the env file to default to .env.dev if no argument specified
+TEST_ENV_FILE="${1:-.env.dev}"
+
+# stop everything and teardown
+docker-compose --env-file "$TEST_ENV_FILE" down
