c59dd85bc0763ae6e9be84c25cf7b92eed53b039
Andrew Varley <av10@sanger.ac.uk>
2022-12-12 19:37:02 +0000

    Trial unit test

---
+++ tolid-api/app/test/unit/test_test.py
@@ -0,0 +1,12 @@
+# SPDX-FileCopyrightText: 2022 Genome Research Ltd.
+#
+# SPDX-License-Identifier: MIT
+
+from __future__ import absolute_import
+
+from unittest import TestCase
+
+class TestTest(TestCase):
+
+    def test_test(self):
+        self.assertEqual(True, False)
