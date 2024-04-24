fbc9ae2dbb1e8e0216515ba7d4ea4fe8db8db9df
Kiernan Harding <kh16@sanger.ac.uk>
2024-04-24 13:05:06 +0100

    search

--- tolid-ui/app/src/pages/Search.tsx
+++ tolid-ui/app/src/pages/Search.tsx
@@ -4,8 +4,71 @@ SPDX-FileCopyrightText: 2024 Genome Research Ltd.
 SPDX-License-Identifier: MIT
 */
 
+import { RemoteTable, Widgets, env } from "@tol/tol-ui";
+
 function Search() {
-  return <></>;
+  const filter = {
+    in_list: {},
+    and_: {
+      "tolid_specimen.id": {exists:{}},
+      "tolid_species.id": {gt: {value: 0}}
+    }
+  };
+
+  const table = (
+    <RemoteTable
+      id="tolid-table-v1"
+      endpoint="tolid"
+      noConfigModal
+      noDownload
+      filter={filter}
+      fields={{
+        "uid": {
+          rename: "ToLID"
+        },
+        "tolid_species.tolid_scientific_name": {
+          rename: "Species Name"
+        },
+        "tolid_species.id": {
+          rename: "Taxonomy ID",
+          cellRenderer: null
+        },
+        "tolid_specimen.id": {
+          rename: "Specimen",
+          cellRenderer: null
+        }
+      }}
+      baseUrl={env.TOL_DATA}
+    />
+  );
+
+  const title = (
+    <div>
+      <h2>Search</h2>
+      <p style={{marginTop: 4}}>
+        Search on a ToLID prefix, taxonomy ID, species name or ToLID.
+      </p>
+    </div>
+  );
+
+  const components = [
+    {
+      component: title,
+      type: 'full'
+    },
+    {
+      component: table,
+      type: 'xl'
+    }
+  ];
+
+  return (
+    <div className="search">
+      <Widgets
+        components={components}
+      />
+    </div>
+  );
 }
 
 export default Search;
