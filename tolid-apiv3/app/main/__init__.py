f35fb1e71faf8d7e63f73251ccbe2b2c7efaa9e7
Edward Moulsdale <em29@sanger.ac.uk>
2024-05-28 13:24:00 +0100

    code skel

--- tolid-apiv3/app/main/__init__.py
+++ tolid-apiv3/app/main/__init__.py
@@ -14,6 +14,8 @@ from tol.sources.goat import goat
 from tol.sql import create_sql_datasource
 from tol.sql.auth import db_auth_blueprint
 
+from .auth import create_auth_inspector
+
 
 def application() -> Flask:
     app = Flask(__name__)
@@ -49,7 +51,8 @@ def application() -> Flask:
     data_bp = data_blueprint(
         sql_ds,
         goat(),
-        url_prefix=f'{api_path}'
+        url_prefix=f'{api_path}',
+        auth_inspector=create_auth_inspector()
     )
     app.register_blueprint(data_bp)
 
---
+++ tolid-apiv3/app/main/auth.py
@@ -0,0 +1,25 @@
+# SPDX-FileCopyrightText: 2024 Genome Research Ltd.
+#
+# SPDX-License-Identifier: MIT
+
+from typing import Optional
+
+from tol.api_base2.auth import AuthInspector
+from tol.api_base2.misc import CtxGetter, default_ctx_getter
+from tol.core.datasource_filter import AndFilter
+from tol.core.operator import OperatorMethod
+
+
+def create_auth_inspector(
+    admin_role: str = 'admin',
+    ctx_getter: CtxGetter = default_ctx_getter
+) -> AuthInspector:
+
+    def __inspector(
+        self,
+        object_type: str,
+        method: OperatorMethod
+    ) -> Optional[AndFilter]:
+        pass
+
+    return __inspector
