b1f3dd8872e9bdf9bd50bb9ec94537f146129839
Edward Moulsdale <em29@sanger.ac.uk>
2024-05-30 11:15:59 +0100

    new test passing

--- tolid-apiv3/app/main/auth.py
+++ tolid-apiv3/app/main/auth.py
@@ -5,9 +5,23 @@
 from typing import Optional
 
 from tol.api_base2.auth import AuthInspector
+from tol.api_base2.auth.error import ForbiddenError
 from tol.api_base2.misc import CtxGetter, default_ctx_getter
 from tol.core.datasource_filter import AndFilter
 from tol.core.operator import OperatorMethod
+
+
+__WRITE_METHODS = [
+    OperatorMethod.DELETE,
+    OperatorMethod.UPDATE,
+    OperatorMethod.UPSERT
+]
+
+
+__ADMIN_ONLY_METHODS = [
+    OperatorMethod.DETAIL,
+    
+]
 
 
 def create_auth_inspector(
@@ -19,6 +33,27 @@ def create_auth_inspector(
         object_type: str,
         method: OperatorMethod
     ) -> Optional[AndFilter]:
-        pass
+
+        auth_ctx = ctx_getter()
+        roles = auth_ctx.roles
+
+        if admin_role in roles:
+            return
+
+        if not roles:
+            if method in __WRITE_METHODS:
+                raise ForbiddenError()
+
+        if method == OperatorMethod.DETAIL:
+            raise ForbiddenError()
+
+        if object_type == 'specimen':
+            return {
+                'user.id': {
+                    'eq': {
+                        'value': auth_ctx.user_id
+                    }
+                }
+            }
 
     return __inspector
