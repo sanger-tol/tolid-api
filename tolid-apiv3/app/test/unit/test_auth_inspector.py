3d7e885dea28479ae82359e8ed9c720cf1a12faf
Edward Moulsdale <em29@sanger.ac.uk>
2024-05-30 08:01:53 +0100

    silly mikstake

--- tolid-apiv3/app/test/unit/test_auth_inspector.py
+++ tolid-apiv3/app/test/unit/test_auth_inspector.py
@@ -10,6 +10,9 @@ from tol.api_base2.misc import (
     AuthContext,
     CtxGetter
 )
+from tol.core.operator import OperatorMethod
+
+from main.auth import create_auth_inspector
 
 
 @pytest.fixture(scope='function')
@@ -36,13 +39,35 @@ class TestAuthInspector:
     `create_auth_inspector()` behaves for various cases
     """
 
-    def test_admin_all(self):
+    def test_admin_all(
+        self,
+        auth_context: AuthContext,
+        ctx_getter: CtxGetter
+    ):
         """
         a user with the `admin` role:
 
         - can perform any operation
         - has no additional filter terms
         """
+
+        auth_context.user_id = '100'
+        auth_context.roles = [
+            'le_admin',
+            'something_else__unimportant'
+        ]
+
+        inspector = create_auth_inspector(
+            admin_role='le_admin',
+            ctx_getter=ctx_getter
+        )
+
+        for op in OperatorMethod:
+            and_term = inspector(
+                'nonsense_type',
+                op
+            )
+            assert not and_term
 
     def test_basic_page_get(self):
         """
