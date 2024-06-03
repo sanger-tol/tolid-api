3aab113f5ad2415f37e2aeedb0c4f6bf1f20545c
Edward Moulsdale <em29@sanger.ac.uk>
2024-06-03 13:04:38 +0100

    noauth too

--- tolid-apiv3/app/main/auth.py
+++ tolid-apiv3/app/main/auth.py
@@ -40,6 +40,7 @@ def create_auth_inspector(
         if op in __WRITE_METHODS:
             raise ForbiddenError()
 
+    @composite.handle_noauth
     @composite.handle
     def __no_forbidden_types(
         object_type: str,
