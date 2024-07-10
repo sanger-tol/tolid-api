3333ef3c5444d6f84c9864582ba005b5bf7e57d8
Logan Howat <lh31@sanger.ac.uk>
2024-07-10 15:51:59 +0100

    TOLP-7792: permit detail get on goat

--- tolid-api/app/main/auth.py
+++ tolid-api/app/main/auth.py
@@ -47,10 +47,13 @@ def create_auth_inspector(
 
     @composite.always
     def __no_detail_get(
-        __object_type: str,
+        object_type: str,
         op: OperatorMethod,
         **kwargs
     ):
+
+        if object_type == 'taxon':
+            return
 
         if op == OperatorMethod.DETAIL:
             raise ForbiddenError()
