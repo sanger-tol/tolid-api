8f19b30430a7c05551c2b5f70a785cdf4524ef94
Nithin <nj9@sanger.ac.uk>
2025-12-08 12:52:09 +0000

    vite update for tolid

---
+++ tolid-ui/app/vite.config.ts
@@ -0,0 +1,44 @@
+/*
+SPDX-FileCopyrightText: 2024 Genome Research Ltd.
+
+SPDX-License-Identifier: MIT
+*/
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import viteTsconfigPaths from "vite-tsconfig-paths";
+import fs from "fs";
+
+// Paths to the key and certificate files
+const keyPath = "/localhost.key";
+const certPath = "/localhost.crt";
+
+// Check if both the key and certificate files exist
+const httpsConfig =
+  fs.existsSync(keyPath) && fs.existsSync(certPath)
+    ? {
+        key: fs.readFileSync(keyPath),
+        cert: fs.readFileSync(certPath),
+      }
+    : false;
+
+export default defineConfig({
+  plugins: [react(), viteTsconfigPaths()],
+  build: {
+    emptyOutDir: true,
+    outDir: "build",
+  },
+  server: {
+    host: "0.0.0.0",
+    port: 3000,
+    https: httpsConfig, // Apply the HTTPS configuration conditionally
+    proxy: {
+      "/api": {
+        target: "http://tolid-api:80",
+        secure: false,
+        changeOrigin: true,
+        ws: true,
+      },
+    },
+  }
+});
