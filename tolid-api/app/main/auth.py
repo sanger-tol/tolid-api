ba999a2c7d57473a952d706bd77de8f40d6cf309
Andrew Varley <av10@sanger.ac.uk>
2026-07-30 09:28:16 +0100

    Updated authinspector

--- tolid-api/app/main/auth.py
+++ tolid-api/app/main/auth.py
@@ -2,61 +2,78 @@
 #
 # SPDX-License-Identifier: MIT
 
-from tol.api_base.auth import AuthInspector
+from typing import Optional
+
+from tol.api_base.auth import CompositeAuthInspector
 from tol.api_base.auth.error import ForbiddenError
-from tol.api_base.misc import (
-    CtxGetter,
-    default_ctx_getter
-)
+from tol.api_base.misc import AuthContext, CtxGetter, default_ctx_getter
 from tol.core.operator import OperatorMethod
 
 
 def create_auth_inspector(
-    ctx_getter: CtxGetter = default_ctx_getter,
-    admin_role: str = 'admin'
-) -> AuthInspector:
+    admin_role: str = 'admin', ctx_getter: CtxGetter = default_ctx_getter
+) -> CompositeAuthInspector:
 
-    WRITE_METHODS = (  # noqa N806
-        OperatorMethod.DELETE,
-        OperatorMethod.INSERT,
-        OperatorMethod.UPDATE,
-        OperatorMethod.UPSERT
+    composite = CompositeAuthInspector(admin_role=admin_role, ctx_getter=ctx_getter)
+
+    composite.forbid_noauth(['user', 'specimen', 'request'])
+
+    @composite.auth(
+        object_type='user',
     )
+    def __user_auth(
+        __object_type: str,
+        op: OperatorMethod,
+        auth_ctx: Optional[AuthContext] = None,
+        bound_args: Optional[object] = None,
+        **kwargs,
+    ):
+        return {'id': {'eq': {'value': auth_ctx.user_id}}}
 
-    def auth_inspector(
-        object_type: str,
-        method: OperatorMethod,
-        *args,
-        **kwargs
-    ) -> None:
+    @composite.auth(
+        object_type='specimen',
+    )
+    def __specimen_auth(
+        __object_type: str,
+        op: OperatorMethod,
+        auth_ctx: Optional[AuthContext] = None,
+        bound_args: Optional[object] = None,
+        **kwargs,
+    ):
+        if op not in [OperatorMethod.PAGE, OperatorMethod.CURSOR]:
+            raise ForbiddenError()
+        return {'user.id': {'eq': {'value': auth_ctx.user_id}}}
 
-        # No access to user
-        if object_type == 'user':
+    @composite.auth(
+        object_type='request',
+    )
+    def __request_auth(
+        __object_type: str,
+        op: OperatorMethod,
+        auth_ctx: Optional[AuthContext] = None,
+        bound_args: Optional[object] = None,
+        **kwargs,
+    ):
+        if op not in [OperatorMethod.PAGE, OperatorMethod.CURSOR]:
+            raise ForbiddenError()
+        return {'user.id': {'eq': {'value': auth_ctx.user_id}}}
+
+    @composite.noauth
+    def __no_write_without_auth(__object_type: str, op: OperatorMethod, **kwargs):
+
+        __WRITE_METHODS = (  # noqa N806
+            OperatorMethod.DELETE,
+            OperatorMethod.INSERT,
+            OperatorMethod.UPDATE,
+            OperatorMethod.UPSERT,
+        )
+
+        if op in __WRITE_METHODS:
             raise ForbiddenError()
 
-        # Only detail get for species and taxon
-        if method == OperatorMethod.DETAIL and object_type not in ['species', 'taxon'] \
-                and (ctx_getter().roles is None or admin_role not in ctx_getter().roles):
+    @composite.always
+    def __no_detail_get(__object_type: str, op: OperatorMethod, **kwargs):
+        if op == OperatorMethod.DETAIL and __object_type not in ['species', 'taxon']:
             raise ForbiddenError()
 
-        # No access to writing if not authenticated
-        if not ctx_getter().authenticated and method in WRITE_METHODS:
-            raise ForbiddenError()
-
-        # No access to specimen if not authenticated
-        if object_type in ['specimen', 'request']:
-            if not ctx_getter().authenticated:
-                raise ForbiddenError()
-            if method not in [OperatorMethod.PAGE, OperatorMethod.CURSOR]:
-                raise ForbiddenError()
-            # Add filter to only see own
-            if ctx_getter().roles is None or admin_role not in ctx_getter().roles:
-                return {
-                    'user.id': {
-                        'eq': {
-                            'value': ctx_getter().user_id
-                        }
-                    }
-                }
-
-    return auth_inspector
+    return composite
