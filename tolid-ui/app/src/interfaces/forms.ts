6946f58523fc58e4eb57c66156970d26a749f99d
Thomas Assheton <ta21@sanger.ac.uk>
2026-09-14 13:24:36 +0100

    Doc comment for IRequestFormData

--- tolid-ui/app/src/interfaces/forms.ts
+++ tolid-ui/app/src/interfaces/forms.ts
@@ -4,7 +4,12 @@ SPDX-FileCopyrightText: 2026 Genome Research Ltd.
 SPDX-License-Identifier: MIT
 */
 
+/**
+ * Values returned from the ToLID request form.
+*/
 export interface IRequestFormData {
+  /** NCBI taxonomy identifier supplied by the user. */
   requestedTaxonomyId: string;
+  /** Internal specimen identifier supplied by the user's lab. */
   specimenId: string;
 }
