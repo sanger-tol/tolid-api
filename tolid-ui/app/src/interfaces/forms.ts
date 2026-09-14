eb1b8625a0b852bce2f2eaab0642e652f74a1238
Thomas Assheton <ta21@sanger.ac.uk>
2026-09-14 15:17:32 +0100

    ninja.ninja

--- tolid-ui/app/src/interfaces/forms.ts
+++ tolid-ui/app/src/interfaces/forms.ts
@@ -4,9 +4,7 @@ SPDX-FileCopyrightText: 2026 Genome Research Ltd.
 SPDX-License-Identifier: MIT
 */
 
-/**
- * Values returned from the ToLID request form.
-*/
+/** Values returned from the ToLID request form */
 export interface IRequestFormData {
   /** NCBI taxonomy identifier supplied by the user. */
   requestedTaxonomyId: string;
